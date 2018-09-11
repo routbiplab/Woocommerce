6553ec6f05f9576dabb3b355478fe22f5491b911
Biplab Rout <routbiplab1993@gmail.com>
2018-09-11 17:17:58 +0530

    fetch customer via updated date modified

--- wp-rest-filter/wp-rest-filter.php
+++ wp-rest-filter/wp-rest-filter.php
@@ -3,12 +3,12 @@
  * Plugin Name: WP REST Filter
  * Description: This plugin helps to fetch customers/orders/products via API using date filter.
  * Author: Biplab Rout
- * Version: 1.0.2
+ * Version: 1.0.3
  **/
 add_action( 'rest_api_init', 'wp_rest_filter_add_filters' );
 function wp_rest_filter_add_filters() {
     /*
-     * Rest Customer API
+     * Rest Customer API v1 and v2
      */
     //../wp-json/wc/v1/customers?created_after=2018-04-20T10:42:35
   add_filter( 'woocommerce_rest_customer_query', function( $args, $request ) {
@@ -16,32 +16,35 @@ function wp_rest_filter_add_filters() {
 	        $args['date_query'][0]['after'] = $request['created_after'];
 	        $args['date_query'][0]['column'] = 'user_registered';
 	    }
-      if( isset( $request['last_update_after'] )) {
-          $args['date_query'][0]['after'] = $request['last_update_after'];
+      //..//wp-json/wc/v2/customers?filter[meta_key]=last_update&filter[meta_value]=2018-09-10T06:43:07&last_update=eq
+      if( $request['filter']['meta_key']=='last_update' ) {
+          $filter = $request['filter'];
+          global $wp;
+          $vars = apply_filters( 'rest_query_vars', $wp->public_query_vars );
+          function allow_meta_query( $valid_vars )
+          {
+              $valid_vars = array_merge( $valid_vars, array( 'meta_query', 'meta_key', 'meta_value', 'meta_compare' ) );
+              return $valid_vars;
+          }
+          $vars = allow_meta_query( $vars );
+          foreach ( $vars as $var ) {
+              if ( isset( $filter[ $var ] ) ) {
+                  $args[ $var ] = $filter[ $var ];
+              }
+          }
+          $dateTime = new DateTime($args[ 'meta_value' ]);
+          $args[ 'meta_value' ] = $dateTime->format('U');
+          if (isset($request['last_update']) && ($request['last_update'] == 'after'))
+              $args[ 'meta_compare' ] = '>';
+          elseif (isset($request['last_update']) && ($request['last_update'] == 'before'))
+              $args[ 'meta_compare' ] = '<';
+          elseif (isset($request['last_update']) && ($request['last_update'] == 'eq'))
+              $args[ 'meta_compare' ] = '=';
       }
+
 	    return $args;
 	}, 10, 2 );
 
-  //../wp-json/wc/v1/customers?last_update_after=2018-05-24T10:42:35
-    add_filter( 'user_search_columns', function( $meta_query_args, $request ) {
-        if( isset( $request['last_update_after'] )) {
-            $dateTime = new DateTime($request['last_update_after']);
-            $compareValue=">";
-            $meta_query_args = array(
-                'relation' => 'AND',
-                array(
-                    'key'     => 'last_update',
-                    'value'   => $dateTime->format('U'),
-                    'compare' => ''.$compareValue.''
-                )
-            );
-            $this->meta_query = new WP_Meta_Query();
-            $this->meta_query->queries=$meta_query_args;
-
-        }
-        return $meta_query_args;
-    }, 10, 2 );
-  
   /*
    * Rest product API.v1
    */  
